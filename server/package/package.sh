8eb64931db00704334a8ee490d65eca8703b1d92
Nikola Spiric <nikola.spiric.ns@gmail.com>
2017-07-15 09:49:56 +0000

    Add all proper files to package.sh script

--- server/package/package.sh
+++ server/package/package.sh
@@ -7,20 +7,31 @@ main() {
     local name="rpis_1.0.0"
     local packageName=${name}.deb
     local rootDir=${name}
+    local installRoot=${rootDir}/usr/local/bin/rpis
+    local projectRoot=`pwd`/..
 
+    # Cleanup
     rm -fv ${packageName}
-
     rm -rfv ${rootDir}
 
     mkdir -p ${rootDir}
+    mkdir -p ${installRoot}
 
-    mkdir -p ${rootDir}/usr/local/bin
 
-    echo "print('hello world')" >> ${rootDir}/usr/local/bin/main.py
+    # Install
+    cp -rv \
+        ${projectRoot}/root \
+        ${projectRoot}/default_config.py \
+        ${projectRoot}/run.sh \
+        ${projectRoot}/start_server.sh \
+        ${projectRoot}/rpis \
+        ${installRoot}/
 
+    # Create the manifest
     mkdir -p ${rootDir}/DEBIAN
     cp control ${rootDir}/DEBIAN/control
 
+    # Create the package
     dpkg-deb --build ${rootDir}
 
 
