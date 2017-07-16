ad0e6e8687a9654667a463f9211b400902af010b
Nikola Spiric <nikola.spiric.ns@gmail.com>
2017-07-16 18:41:18 +0200

    [client] Disable swipe scrolling trough the pager

---
+++ client/app/src/main/java/com/rpis/client/navigator/CustomPager.java
@@ -0,0 +1,22 @@
+package com.rpis.client.navigator;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class CustomPager extends ViewPager {
+    public CustomPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return false;
+    }
+}
